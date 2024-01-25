708f550c551af88470498b27649bea41ccfd85f4
ava <a.bakuie@adpdigital.com>
2024-01-25 20:26:11 +0100

    edit layout

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "https://www.thoughtco.com/thmb/rn4UnyqlJMWo5mqpEOYbenaOjWA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/old-fortress-gate-with-light-trails-at-downtown-455242307-58dea6143df78c5162e1ff3d.jpg",
+        title: "South Korea",
+        location: "Asia",
+        description: "32 Deals",
+    },
+    {
+        id: 2,
+        src: "https://assets.adac.de/image/upload/c_scale,f_auto,q_auto,t_2:1-default,w_1500/v1/ADAC-eV/KOR/Bilder/RF/london-aufmacher-2204_bc0xmh",
+        title: "London",
+        location: "United Kingdom",
+        description: "45 Deals",
+    },
+    {
+        id: 3,
+        src: "https://www.polilingua.com/uploads/posts/singapore-language-3.jpg",
+        title: "Singapore",
+        location: "Asia",
+        description: "88 Deals",
+    },
+    {
+        id: 4,
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg",
+        title: "New York City",
+        location: "United State of America",
+        description: "92 Deals",
+    },
+    {
+        id: 5,
+        src: "https://www.tiqets.com/blog/wp-content/uploads/2023/05/berlin-in-3-days-1-1024x570.jpg",
+        title: "Berlin",
+        location: "Germany",
+        description: "15 Deals",
+    },
+]
